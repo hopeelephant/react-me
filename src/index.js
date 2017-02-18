16316efab9cca3d1dc4362461920a20cefc996ca
hopeelephant <765846396@qq.com>
2017-02-18 22:15:16 +0800

    react

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+function Hello(){
+
+  return <div>
+          <h1>hello</h1>
+          <div>{a}</div>
+          <div>{a}</div>
+          <div>{a}</div>
+          <div>{a}</div>
+          <div>{a}</div>
+          <div>{a}</div>
+          <div>{a}</div>
+        </div>
+
+}
+let a = <h1>hello11111111s</h1>;
+ReactDOM.render(
+  <Hello />,document.getElementById("app")
+)
